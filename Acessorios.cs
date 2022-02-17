b589068f7b6d44bebc6504ce04d12ae7bb60a06b
Tiago Liberato <tiagolfsm@gmail.com>
2022-02-16 21:19:24 -0300

    First Commit

---
+++ Acessorios.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Carro
+{
+    public class Acessorios
+    {
+        public static Volante Autoesporte(int Ano, string Nome, string Marca)
+        {
+            Volante volante = new Volante();
+            volante.Ano = Ano;
+            volante.Nome = Nome;
+            volante.Marca = Marca;
+            return volante;
+        }
+        public static List<Volante> ListCount(int Ano, string Nome, string Marca)
+        {
+            List<Volante> lista = new List<Volante>();
+            Volante volante = new Volante();
+            volante.Nome = Nome;
+            volante.Marca = Marca;
+            volante.Ano = Ano;
+            lista.Add(volante);
+
+            Volante volante1 = new Volante();
+            volante1.Nome = "Opala";
+            volante1.Marca = "Chevrolet";
+            volante1.Ano = 1978;
+            lista.Add(volante1);
+
+            Volante volante2 = new Volante();
+            volante2.Nome = "Palio";
+            volante2.Ano = 2006;
+            volante2.Marca = "Fiat";
+            lista.Add(volante2);
+
+            Volante volante3 = new Volante();
+            volante3.Nome = "Escorte";
+            volante3.Marca = "Ford";
+            volante3.Ano= 1998;
+            lista.Add(volante3);
+
+            Volante volante4 = new Volante();
+            volante4.Nome = "Fusion";
+            volante4.Ano = 2010;
+            volante4.Marca = "Ford";
+            lista.Add(volante4);
+
+            Volante volante5 = new Volante();
+            volante5.Nome = "MErcedez";
+            volante5.Marca = "Mercedez";
+            volante5.Ano = 2022;
+            lista.Add(volante5);
+
+            return lista;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Carro;
+
+
+namespace carro
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===========Carros Do Ano=================== ");
+            Volante Prisma = Acessorios.Autoesporte(2005, "Apollo", "Ford");
+            Console.WriteLine("Seu carro é:" + Prisma.Nome);
+            Console.WriteLine("Sua Marca é:" + Prisma.Marca);
+            Console.WriteLine("Seu Ano é:" + Prisma.Ano);
+
+            Console.WriteLine("=============Proximos Carros================");
+            List<Volante> lista=Acessorios.ListCount(2005,"Veloster","Hyunday");
+
+
+
+            foreach (Volante ListCount in lista) {
+                Console.WriteLine("Nome :  " + ListCount.Nome);
+                Console.WriteLine("Marca :" + ListCount.Marca);
+                Console.WriteLine("Ano : " + ListCount.Ano);
+                Console.WriteLine("-------------------------------------");
+
+            }
+
+
+
+            /*lista.ForEach(ListCount => {
+                             
+                Console.WriteLine("Nome :  "  + ListCount. Nome );
+                Console.WriteLine("Marca :" + ListCount.Marca);
+                Console.WriteLine("Ano : " + ListCount.Ano);
+                Console.WriteLine("-------------------------------------");
+                
+                
+
+            });*/
+        }
+    }
+}
